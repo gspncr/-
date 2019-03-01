d2d49d1537f8e7c2dcd24edd396e262ed5dd71fa
Gary <gspenceruk+git@gmail.com>
2019-03-01 20:34:12 +0000

    (1) edit a site, (2) API view / delete #4 #8 #10
    You can now modify a site in a sites page
    You can now call the JSONified API for consuming data
    You can now call the remove API to remove a site

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 print(os.environ['FS_DB'])
@@ -35,11 +35,6 @@ def index():
     latest = Site.query.order_by(Site.id.desc()).first()
     return render_template('app.html', refSite=refSite, entries=entries, latest=latest)
 
-@app.route('/site/<sitename>')
-def site(sitename):
-    oneSite = Site.query.filter_by(site=sitename).first()
-    return render_template('site.html', oneSite=oneSite)
-
 @app.route('/new-site', methods=['POST'])
 def newsite():
     try:
@@ -51,5 +46,59 @@ def newsite():
         e = "Site already exists!"
         return render_template('error.html', error=e)
 
+@app.route('/update/<sitename>', methods=['POST'])
+def updateSite(sitename):
+    try:
+        db.session.query(Site).filter_by(site=sitename).update({'score': request.form['score'], 'availabilityScore': request.form['availabilityScore'], 'cleanlinessScore': request.form['cleanlinessScore'], 'lightingScore': request.form['lightingScore'], 'spaciousScore': request.form['spaciousScore']})
+        db.session.commit()
+        return redirect('site/'+sitename)
+    except exc.IntegrityError as e:
+        e = "unhandled"
+        return render_template('error.html', error=e)
+
+@app.route('/remove/<sitename>', methods=['DELETE'])
+def removeSite(sitename):
+    try:
+        site = Site.query.filter_by(site=sitename).delete()
+        db.session.commit()
+        return redirect(url_for('index'))
+    except exc.IntegrityError as e:
+        e = "Site already exists!"
+        return render_template('error.html', error=e)
+
+@app.route('/site/<sitename>', methods=['GET'])
+def site(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return render_template('site.html', oneSite=oneSite)
+
+@app.route('/api/site/<sitename>', methods=['GET'])
+def sitelookup(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(name=oneSite.site, overall=oneSite.score, availability=oneSite.availabilityScore, cleanliness=oneSite.cleanlinessScore, lighting=oneSite.lightingScore, space=oneSite.spaciousScore)
+
+@app.route('/api/score/<sitename>', methods=['GET'])
+def sitescore(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(overall=oneSite.score)
+
+@app.route('/api/availability/<sitename>', methods=['GET'])
+def availability(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(availability=oneSite.availabilityScore)
+
+@app.route('/api/cleanliness/<sitename>', methods=['GET'])
+def cleanliness(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(cleanliness=oneSite.cleanlinessScore)
+
+@app.route('/api/lighting/<sitename>', methods=['GET'])
+def lighting(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(lighting=oneSite.lightingScore)
+
+@app.route('/api/space/<sitename>', methods=['GET'])
+def space(sitename):
+    oneSite = Site.query.filter_by(site=sitename).first()
+    return jsonify(space=oneSite.spaciousScore)
 
 app.run(host='0.0.0.0', port=8080)
