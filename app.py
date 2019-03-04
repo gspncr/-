13a7ace68b37ff0f9e6d496117cf5561956b6c59
Gary <gspenceruk+git@gmail.com>
2019-03-04 17:45:22 +0000

    API enhancements
    stopped json alphabet sorting && introduced:
    GET:
    /latest-10
    /cleanest-10
    /top-overall-10
    /most-available-10
    /top-lighting-10
    /most-spacious-10

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ print(os.environ['FS_DB'])
 app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ['FS_DB']
+app.config['JSON_SORT_KEYS'] = False
 db = SQLAlchemy(app)
 
 class Site(db.Model):
@@ -34,6 +35,113 @@ def index():
     entries = Site.query.count()
     latest = Site.query.order_by(Site.id.desc()).first()
     return render_template('app.html', refSite=refSite, entries=entries, latest=latest)
+
+@app.route('/latest-10')
+def latestTen():
+    refSite = Site.query.limit(10).all()
+    return jsonify("latest-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
+
+@app.route('/cleanest-10')
+def cleanestTen():
+    #results = db.session.query(User).filter(User.name == "Bob").order_by(User.age.desc()).limit(10)
+    refSite = Site.query.order_by(Site.cleanlinessScore.desc()).limit(10).all()
+    return jsonify("cleanest-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
+
+@app.route('/top-overall-10')
+def topOverallTen():
+    #results = db.session.query(User).filter(User.name == "Bob").order_by(User.age.desc()).limit(10)
+    refSite = Site.query.order_by(Site.score.desc()).limit(10).all()
+    return jsonify("top-overall-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
+
+@app.route('/most-available-10')
+def mostAvailableTen():
+    #results = db.session.query(User).filter(User.name == "Bob").order_by(User.age.desc()).limit(10)
+    refSite = Site.query.order_by(Site.availabilityScore.desc()).limit(10).all()
+    return jsonify("most-available-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
+
+@app.route('/top-lighting-10')
+def topLightingTen():
+    #results = db.session.query(User).filter(User.name == "Bob").order_by(User.age.desc()).limit(10)
+    refSite = Site.query.order_by(Site.lightingScore.desc()).limit(10).all()
+    return jsonify("top-lighting-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
+
+@app.route('/most-spacious-10')
+def mostSpaciousTen():
+    #results = db.session.query(User).filter(User.name == "Bob").order_by(User.age.desc()).limit(10)
+    refSite = Site.query.order_by(Site.spaciousScore.desc()).limit(10).all()
+    return jsonify("most-spacious-10",
+        [dict(site=refSite[0].site, score=refSite[0].score, availability=refSite[0].availabilityScore, cleanliness=refSite[0].cleanlinessScore, lighting=refSite[0].lightingScore, space=refSite[0].spaciousScore),
+        dict(site=refSite[1].site, score=refSite[1].score, availability=refSite[1].availabilityScore, cleanliness=refSite[1].cleanlinessScore, lighting=refSite[1].lightingScore, space=refSite[1].spaciousScore),
+        dict(site=refSite[2].site, score=refSite[2].score, availability=refSite[2].availabilityScore, cleanliness=refSite[2].cleanlinessScore, lighting=refSite[2].lightingScore, space=refSite[2].spaciousScore),
+        dict(site=refSite[3].site, score=refSite[3].score, availability=refSite[3].availabilityScore, cleanliness=refSite[3].cleanlinessScore, lighting=refSite[3].lightingScore, space=refSite[3].spaciousScore),
+        dict(site=refSite[4].site, score=refSite[4].score, availability=refSite[4].availabilityScore, cleanliness=refSite[4].cleanlinessScore, lighting=refSite[4].lightingScore, space=refSite[4].spaciousScore),
+        dict(site=refSite[5].site, score=refSite[5].score, availability=refSite[5].availabilityScore, cleanliness=refSite[5].cleanlinessScore, lighting=refSite[5].lightingScore, space=refSite[5].spaciousScore),
+        dict(site=refSite[6].site, score=refSite[6].score, availability=refSite[6].availabilityScore, cleanliness=refSite[6].cleanlinessScore, lighting=refSite[6].lightingScore, space=refSite[6].spaciousScore),
+        dict(site=refSite[7].site, score=refSite[7].score, availability=refSite[7].availabilityScore, cleanliness=refSite[7].cleanlinessScore, lighting=refSite[7].lightingScore, space=refSite[7].spaciousScore),
+        dict(site=refSite[8].site, score=refSite[8].score, availability=refSite[8].availabilityScore, cleanliness=refSite[8].cleanlinessScore, lighting=refSite[8].lightingScore, space=refSite[8].spaciousScore),
+        dict(site=refSite[9].site, score=refSite[9].score, availability=refSite[9].availabilityScore, cleanliness=refSite[9].cleanlinessScore, lighting=refSite[9].lightingScore, space=refSite[9].spaciousScore)
+        ]
+    )
 
 @app.route('/new-site', methods=['POST'])
 def newsite():
